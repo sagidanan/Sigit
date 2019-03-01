75340abf8d796d8b57bf29d9b021ff4f0519d45c
sagidanan <sagidanan1@gmail.com>
2019-03-01 21:11:07 +0200

    added exceptions handling - surronded code with try-except

--- exercise1.py
+++ exercise1.py
@@ -6,6 +6,41 @@ code = "1608"   # the secret code
 money = 10000   # the amount of the money in the start
 
 
+def user_input(start, end):
+    """
+    this function will get an input from the user in some range(from x to y)
+    :param start: the 'from' of the input
+    :param end: the 'to' of the input
+    :return: the user input
+    """
+    while True:
+        data = input("Please enter what would you want to do: ")
+        try:
+            data = int(data)    # if the user input is letter - exception will thrown
+            if data < start or data > end:
+                print("Error!! please enter a number between " + str(start) + " to " + str(end))
+            else:
+                return data
+        except ValueError:
+            print("Error!! please enter a number between " + str(start) + " to " + str(end))
+
+
+def check_secret_code():
+    """
+    this function will ask the user what is the secret code
+    and the function will check if the code he entered is correct
+    :return: True - the user input is correct
+                otherwise - False
+    """
+    global code
+    secret_code = input("Please enter your secret code: ")
+    if secret_code == code:
+        return True
+    else:
+        print("wrong code")
+        return False
+
+
 def print_menu():
     """
     this function will print the menu
@@ -36,22 +71,6 @@ def menu_withdrawal():
     print("4.   back to menu")
 
 
-def check_secret_code():
-    """
-    this function will ask the user what is the secret code
-    and the function will check if the code he entered is correct
-    :return: True - the user input is correct
-                otherwise - False
-    """
-    global code
-    secret_code = input("Please enter your secret code: ")
-    if secret_code == code:
-        return True
-    else:
-        print("wrong code")
-        return False
-
-
 def remove_from_money(amount):
     """
     this function remove amount of money from my total money
@@ -71,16 +90,18 @@ def cache_withdrawal():
     :return:
     """
     menu_withdrawal()
-    choice = input("Please enter your choice: ")
-    choice = int(choice)
+    choice = user_input(1, 4)
     if choice == 1:
         remove_from_money(20)
     elif choice == 2:
         remove_from_money(50)
     elif choice == 3:
-        amount = input("Please enter the amount of the money do you want to withdrawal: ")
-        amount = int(amount)
-        remove_from_money(amount)
+        try:
+            amount = input("Please enter the amount of the money do you want to withdrawal: ")
+            amount = int(amount)
+            remove_from_money(amount)
+        except ValueError:
+            print("Error input!!")
 
 
 def exchange_secret_code():
@@ -88,9 +109,13 @@ def exchange_secret_code():
     this function will change the secret code
     :return: None
     """
-    new_code = input("Please enter the new secret code: ")
     global code
-    code = new_code
+    try:
+        new_code = input("Please enter the new secret code: ")
+        int(new_code)   # if it will throw exception its mean that the user input isn't contain only numbers
+        code = new_code
+    except ValueError:
+        print("code needs to be contain only numbers")
 
 
 def main():
@@ -99,17 +124,17 @@ def main():
     """
     while True:
         print_menu()
-        user_input = input("Please enter what would you want to do: ")
-        if user_input == "4":
+        data = user_input(1, 4)
+        if data == 4:
             print("Thanks goodbye")
             break
         else:
             if check_secret_code():
-                if user_input == "1":
+                if data == 1:
                     print_balance()
-                elif user_input == "2":
+                elif data == 2:
                     cache_withdrawal()
-                elif user_input == "3":
+                elif data == 3:
                     exchange_secret_code()
             print()
 
