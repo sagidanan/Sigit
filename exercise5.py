8ff515a2857cbe40decdd99c218f528226e7fc8e
sagidanan <sagidanan1@gmail.com>
2019-03-08 21:17:42 +0200

    added exceptions handeling - try_except

--- exercise5.py
+++ exercise5.py
@@ -12,11 +12,19 @@ def check_id(il_id):
     """
     mask = [1, 2, 1, 2, 1, 2, 1, 2]
     part = il_id[:-1]
-    checksum = int(il_id[-1:])
+    if len(il_id) != 9:
+        raise OverflowError
+    try:
+        checksum = int(il_id[-1:])
+    except ValueError:
+        raise ValueError
     sum_id = 0
     for i in range(len(part)):
-        mul = mask[i] * int(part[i])
-        mul = int(mul)
+        try:
+            mul = mask[i] * int(part[i])
+            mul = int(mul)
+        except ValueError:
+            raise ValueError
         if mul > 9:  # if the number has 2 digits - sum the digit
             mul -= 9
         sum_id += mul
@@ -24,7 +32,12 @@ def check_id(il_id):
 
 
 def main():
-    print(check_id("211863683"))
+    try:
+        print(check_id("211863683"))
+    except ValueError:
+        print("Error - Id has to contain numbers only")
+    except OverflowError:
+        print("ID must to be 9 numbers")
 
 
 if __name__ == "__main__":
