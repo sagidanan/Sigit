806266ef3e4f5fd5dba6111c3f2fa6d36d79923b
sagidanan <sagidanan1@gmail.com>
2019-03-08 14:07:47 +0200

    Fixed split bug, also added more comments

--- exercise2.py
+++ exercise2.py
@@ -1,45 +1,49 @@
+"""
+Exercise 2
+"""
 
 
-def calculate_list(list):
+def calculate_list(list_to_sum):
     """
     this function will sum the list
-    :param list: the list - contain int only
+    :param list_to_sum: the list - contain int only
     :return: the sum of the list
     """
-    return sum(list)
+    return sum(list_to_sum)
 
 
 def option_a():
     """
-    this function impliment the first option
+    this function implement the first option
     the user enter number ofter number (until he enters 'stop') and then it will sum all his inputs and print it
     :return: None
     """
-    list = []
+    num_list = []
     while True:
         num = input("Please enter a number to add to the list ('stop' to stop): ")
         if num.lower() == "stop":
             break
         try:
             num = int(num)  # convert the input to integer
-            list.append(num)    # add his input to the list
+            num_list.append(num)  # add his input to the list
         except ValueError:
             print("Must to be a number or 'stop'!!!")
 
-    print("the sum is: ", calculate_list(list)) # sum the list
+    print("the sum is: ", calculate_list(num_list))  # sum the list
 
 
 def option_b():
     """
-    this function impliment the second option
+    this function implement the second option
     the user enter the list like '1,2,3,4' and then we will sum the list and print the result
     :return: None
     """
     str_list = input("Please enter the list - split with ',' : ")
-    list = split(str_list, ",") # split the string by ',' and convert it to list
+    num_list = str_list.split(",") # split the string by ',' and convert it to list
     try:
-        list = [int(x) for x in list]  # for each value in the list, we will convert it to a number (from string)
-        print("the sum is: ", calculate_list(list))
+        num_list = [int(x) for x in num_list]   # for each value in the list
+                                                # we will convert it to a number (from string)
+        print("the sum is: ", calculate_list(num_list))
     except ValueError:
         print("list must contain only numbers and ',' !!")
 
@@ -66,7 +70,8 @@ def main():
         if choice == 3:
             print("Thanks Bye")
             break
-        strategy[choice]() # call to the function by the choice - used strategy design pattern
+        strategy[choice]()  # call to the function by the choice - used strategy design pattern
+
 
 if __name__ == "__main__":
     main()
